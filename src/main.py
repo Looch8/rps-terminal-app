f4498bb3ea262e62b15d23bb2fdcd6ea3e5e7fa8
Looch8 <lukewheldale@gmail.com>
2023-04-24 21:01:41 +0930

    initial commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# ROCK PAPER SCISSORS
+
+# Function that gets user input
+# Function that gets generates a computer choice
+# Function that plays one round of RPS.
+# Function that keeps score (first to 5 wins) and prints winner to the console
+# Ability to reset game at any point in time during the game
+# Function to reset game and start again once a winner is announced
+# score function in file that updates with each round (thanks for suggestion, Simon)
