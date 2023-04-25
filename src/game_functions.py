b403d153e0b3eb3c07598840ff3b48a80c56473e
Looch8 <lukewheldale@gmail.com>
2023-04-25 13:22:52 +0930

    finished game, csv file not opening, have to figure out why

--- src/game_functions.py
+++ src/game_functions.py
@@ -1,35 +1,3 @@
 # Modules
 import random  # for getting random computer choice
-
-# Function to get player choice, will raise error if use does not enter either 'rock', 'paper', or 'scissors'. Input is not case sensitive
-
-# Player choice
-
-
-def get_player_choice():
-    while True:
-        player_choice = input('rock, paper, or scissors?: ').lower()
-        if player_choice not in ['rock', 'paper', 'scissors']:
-            # Raise value error if not an input from list
-            raise ValueError('Not a valid choice')
-        else:
-            return player_choice
-
-# Computer choice
-
-
-def get_computer_choice():
-    computer_choices = ['rock', 'paper', 'scissors']
-    computer_choice = random.choice(computer_choices)
-    return computer_choice
-
-# Round of RPS
-
-
-def play_round(player_choice, computer_choice):
-    if player_choice == computer_choice:
-        return ('Draw!')
-    elif player_choice == 'rock' and computer_choice == 'scissors' or player_choice == 'paper' and computer_choice == 'rock' or player_choice == 'scissors' and computer_choice == 'paper':
-        return f'You win - {player_choice} beats {computer_choice}!'
-    else:
-        return f'You lose = {computer_choice} beats {player_choice}!'
+import csv
--- src/main.py
+++ src/main.py
@@ -7,12 +7,101 @@ import sys  # provide info on functions
 import unittest  # for testing
 
 # Function imports
-from game_functions import get_player_choice, get_computer_choice, play_round
+
+# # Create CSV file
+# with open('score.csv', 'w') as csv_file:
+#     fieldnames = ['Player', 'Computer', 'Score']
+#     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#     writer.writeheader()
+
+# Append player and computer information to CSV file
+#  with open('score.csv', 'a') as csv.file:
+#         writer = csv.writer(csv_file)
+#         writer.writerow(['Player', player_score])
+#         writer.writerow(['Computer', computer_score])
+
+# Function to get player choice, will raise error if use does not enter either 'rock', 'paper', or 'scissors'. Input is not case sensitive
+
+# Player choice
 
 
-print(get_player_choice())
-print(get_computer_choice())
-print(play_round(player_choice='rock', computer_choice='scissors'))
+def get_player_choice():
+    while True:
+        player_choice = input('rock, paper, or scissors?: ').lower()
+        if player_choice not in ['rock', 'paper', 'scissors']:
+            # Raise value error if not an input from list
+            raise ValueError('Not a valid choice')
+        else:
+            return player_choice
+
+# Computer choice
+
+
+def get_computer_choice():
+    computer_choices = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(computer_choices)
+    return computer_choice
+
+# Round of RPS
+
+
+def play_round(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return ('draw')
+    elif player_choice == 'rock' and computer_choice == 'scissors' or player_choice == 'paper' and computer_choice == 'rock' or player_choice == 'scissors' and computer_choice == 'paper':
+        return 'player'
+        # return f'You win - {player_choice} beats {computer_choice}!'
+    else:
+        return 'computer'
+        # return f'You lose - {computer_choice} beats {player_choice}!'
+
+
+def save_scores(player_score, computer_score):
+    with open('score.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([player_score, computer_score])
+
+
+def print_scores():
+    with open('score.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        player_wins = 0
+        computer_wins = 0
+        for row in reader:
+            players_wins += int(row[0])
+            computer_wins += int(row[1])
+        print(f'Player wins: {player_wins}')
+        print(f'Computer wins: {computer_wins}')
+
+
+def game():
+    # Initial scores
+    player_score = 0
+    computer_score = 0
+    while True:
+        player_choice = get_player_choice()
+        computer_choice = get_computer_choice()
+        winner = play_round(player_choice, computer_choice)
+        if winner == 'tie':
+            return "It's a draw!"
+        elif winner == 'player':
+            player_score += 1
+            return f"You win - {player_choice} beats {computer_choice}"
+        else:
+            computer_score += 1
+            return f"You lose - {computer_choice} beats {player_choice}"
+        save_scores(player_score, computer_score)
+        print_scores()
+
+    # # Loop number of rounds
+    # for current_round in range(1, 6):
+    #     print(f'Round {current_round}')
+    #     play_round(player_choice='rock', computer_choice='scissors')
+    #     current_round += 1
+
+
+game()
+
 
 # User choice
 # player_selection = input('rock, paper, or scissors? ').lower()
