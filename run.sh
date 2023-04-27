abab32acf217cf1dccd5bc270091f78d1557b112
Looch8 <lukewheldale@gmail.com>
2023-04-27 15:30:44 +0930

    added a second test case

--- run.sh
+++ run.sh
@@ -5,3 +5,9 @@ python3 -m venv rps-venv
 source rps-venv/bin/activate
 pip3 install -r requirements.txt
 python3 src/main.py
+
+# to run test (While in rps-venv)
+# python -m pytest 
+
+# To run .sh file
+# sh run.sh
--- src/test_main.py
+++ src/test_main.py
@@ -1,46 +1,39 @@
 import pytest  # for testing
-from unittest import mock
-from main import play_round
+from main import play_round, get_computer_choice
+# from unittest import mock
 
+# Test the play_round function
 
-# @mock.patch('main.play_round', return_value='rock', autospec=True)
-# def test_play_round(mock_play_round):
-#     assert play_round('scissors') == 'player'
-
-# def test_play_round(monkeypatch):
-#     monkeypatch.setattr('builtins.input', lambda _: 'rock')
-#     assert play_round()
-# Play_round function testing
-# def test_play_round(monkeypatch):
-#     # Player wins
-#     assert play_round('rock', 'scissors') == 'player'
-# assert play_round('paper', 'rock') == 'player'
-# assert play_round('scissors', 'paper') == 'player'
-
-# # Computer wins
-# assert play_round('rock', 'paper') == 'computer'
-# assert play_round('paper', 'scissors') == 'computer'
-# assert play_round('scissors', 'rock') == 'computer'
-
-# # draw
-# assert play_round('rock', 'rock') == 'draw'
-# assert play_round('paper', 'paper') == 'draw'
-# assert play_round('scissors', 'scissors') == 'draw'
 
 def test_play_round():
     # Player wins
     assert play_round('rock', 'scissors') == 'player'
+    assert play_round('paper', 'rock') == 'player'
+    assert play_round('scissors', 'paper') == 'player'
+
+    # Computer wins
+    assert play_round('rock', 'paper') == 'computer'
+    assert play_round('paper', 'scissors') == 'computer'
+    assert play_round('scissors', 'rock') == 'computer'
+
+    # draw
+    assert play_round('rock', 'rock') == 'draw'
+    assert play_round('paper', 'paper') == 'draw'
+    assert play_round('scissors', 'scissors') == 'draw'
+
+# Test the get_player_choice function
 
 
-assert play_round('paper', 'rock') == 'player'
-assert play_round('scissors', 'paper') == 'player'
+# Test the get_computer_choice function - using monkeypatch to replace random.choice function and with a lambda function to always return a predefined string.
+def test_get_computer_choice(monkeypatch):
+    # Test case 1: simulate the computer choosing 'rock'
+    monkeypatch.setattr('random.choice', lambda x: 'rock')
+    assert get_computer_choice() == 'rock'
 
-# Computer wins
-assert play_round('rock', 'paper') == 'computer'
-assert play_round('paper', 'scissors') == 'computer'
-assert play_round('scissors', 'rock') == 'computer'
+    # Test case 2: simulate the computer choosing 'paper'
+    monkeypatch.setattr('random.choice', lambda x: 'paper')
+    assert get_computer_choice() == 'paper'
 
-# draw
-assert play_round('rock', 'rock') == 'draw'
-assert play_round('paper', 'paper') == 'draw'
-assert play_round('scissors', 'scissors') == 'draw'
+    # Test case 3: simulate the computer choosing 'scissors'
+    monkeypatch.setattr('random.choice', lambda x: 'scissors')
+    assert get_computer_choice() == 'scissors'
